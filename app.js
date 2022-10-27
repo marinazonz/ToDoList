fbdd88359382f9c0aba761f0ac402d656997d9df
marinazonz <marinazonz@gmail.com>
2022-10-27 18:31:14 +0200

    second commit

--- app.js
+++ app.js
@@ -4,8 +4,7 @@ const userName = document.querySelector("#userName");
 const newToDoForm = document.querySelector("#new-todo-form");
 const divListToDo = document.querySelector(".list");
 const divItemToDo = document.querySelector(".todo-item");
-const btnEditTodolist = document.querySelector(".edit");
-const btnDeleteTodolist = document.querySelector(".delete");
+const divItemContent = document.querySelector(".todo-conten");
 
 window.onload = function () {
     //Get user name from storage
@@ -13,7 +12,9 @@ window.onload = function () {
     userName.value = userNameStored;
 
     //Render saved todolist
-    renderTodos();
+    if (toDoList) {
+        renderTodos();
+    }
 };
 
 //get todo list from the storage
@@ -25,7 +26,7 @@ userName.addEventListener("change", function (e) {
     localStorage.setItem("username", JSON.stringify(e.target.value));
 });
 
-//Store and render toDo List
+//Add and store a new toDo
 newToDoForm.addEventListener("submit", (e) => {
     e.preventDefault();
 
@@ -47,13 +48,14 @@ newToDoForm.addEventListener("submit", (e) => {
     renderTodos();
 });
 
+//render Todo List
 const renderTodos = function () {
     divListToDo.innerHTML = "";
 
     toDoList.forEach((todo) => {
         const html = `<div class="todo-item">
                     <label>
-                        <input type="checkbox" />
+                        <input type="checkbox" id="inputCheckbox" />
                         <span class="bubble ${
                             todo.category === "business"
                                 ? "business"
@@ -77,11 +79,58 @@ const renderTodos = function () {
 
         divListToDo.insertAdjacentHTML("afterbegin", html);
 
+        const btnEditTodolist = document.querySelector(".edit");
+        const btnDeleteTodolist = document.querySelector(".delete");
+
         //Delete todolist item
-        //btnDeleteTodolist.addEventListener("click", deletetodoListItem);
+        btnDeleteTodolist.addEventListener("click", deletetodoListItem);
+
+        //Edit todolist item
+        btnEditTodolist.addEventListener("click", edittodoListItem);
     });
 };
 
 const deletetodoListItem = function (e) {
-    console.log(e);
+    const element = e.path[2];
+    element.remove();
+
+    const deletedElement = e.path[2].children[1].childNodes[1].value;
+    const newToDoList = toDoList.filter(
+        (data) => data.content != deletedElement
+    );
+
+    localStorage.setItem("todos", JSON.stringify(newToDoList));
+};
+
+const edittodoListItem = function (e) {
+    const input = e.path[2].children[1].childNodes[1];
+    // const input = e.target;
+    input.removeAttribute("readonly");
+    input.focus();
+
+    const spanBubble = e.path[2].children[0].children[1];
+
+    if (spanBubble.classList.contains("personal")) {
+        input.style.color = "#ea40a4";
+        input.style.fontSize = "1.5rem";
+        input.style.transition = "0.2s ease-in-out";
+    } else {
+        input.style.color = "#3a82ee";
+        input.style.fontSize = "1.5rem";
+        input.style.transition = "0.2s ease-in-out";
+    }
+    setTimeout(function () {
+        input.style.color = "var(--dark)";
+        input.style.fontSize = "1.125rem";
+    }, 400);
+
+    input.addEventListener("blur", (e) => {
+        input.setAttribute("readonly", true);
+        console.log(e.target.value);
+        console.log(e);
+
+        //make localStorage works
+        //check correct pathes
+        //localStorage.setItem("todos", JSON.stringify(toDoList));
+    });
 };
