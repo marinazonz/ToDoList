7cb949cf0338d35f32a80a93c2e2a5f4484be6b4
marinazonz <marinazonz@gmail.com>
2022-10-28 13:24:14 +0200

    added greeting depending on the time and worked on deleteBtn

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 "use strict";
 
-const userName = document.querySelector("#userName");
+const greeting = document.querySelector(".greeting");
 const newToDoForm = document.querySelector("#new-todo-form");
 const divListToDo = document.querySelector(".list");
 const divItemToDo = document.querySelector(".todo-item");
 const divItemContent = document.querySelector(".todo-conten");
 
+//date
+const date = new Date();
+
 window.onload = function () {
+    //greeting
+    greetingTime();
+
     //Get user name from storage
     const userNameStored = JSON.parse(localStorage.getItem("username")) || "";
     userName.value = userNameStored;
@@ -19,12 +25,6 @@ window.onload = function () {
 
 //get todo list from the storage
 const toDoList = JSON.parse(localStorage.getItem("todos")) || [];
-
-//Store user name
-userName.addEventListener("change", function (e) {
-    //console.log(e.target.value);
-    localStorage.setItem("username", JSON.stringify(e.target.value));
-});
 
 //Add and store a new toDo
 newToDoForm.addEventListener("submit", (e) => {
@@ -65,6 +65,7 @@ const renderTodos = function () {
 
                     <div class="todo-content">
                         <input
+                            id="inputValue"
                             type="text"
                             value='${todo.content}'
                             readonly
@@ -81,6 +82,7 @@ const renderTodos = function () {
 
         const btnEditTodolist = document.querySelector(".edit");
         const btnDeleteTodolist = document.querySelector(".delete");
+        const bubble = document.querySelector(".bubble");
 
         //Delete todolist item
         btnDeleteTodolist.addEventListener("click", deletetodoListItem);
@@ -91,10 +93,16 @@ const renderTodos = function () {
 };
 
 const deletetodoListItem = function (e) {
-    const element = e.path[2];
+    let current = document.querySelector(".delete");
+    const element = current.parentNode.parentNode;
+    //console.log(e.path[2].children[1].childNodes[1].value);
+
+    let input = document.querySelector("#inputValue");
+
     element.remove();
 
-    const deletedElement = e.path[2].children[1].childNodes[1].value;
+    const deletedElement = input.value;
+
     const newToDoList = toDoList.filter(
         (data) => data.content != deletedElement
     );
@@ -103,12 +111,18 @@ const deletetodoListItem = function (e) {
 };
 
 const edittodoListItem = function (e) {
+    let current = document.querySelector(".edit");
+
+    console.log(current.parentNode);
+
+    //const input = current.parentNode.previousElementSibling.firstElementChild;
     const input = e.path[2].children[1].childNodes[1];
     // const input = e.target;
     input.removeAttribute("readonly");
     input.focus();
 
     const spanBubble = e.path[2].children[0].children[1];
+    // console.log(current.parentElement.previousElementSibling.previousElementSibling.lastElementChild)
 
     if (spanBubble.classList.contains("personal")) {
         input.style.color = "#ea40a4";
@@ -130,7 +144,37 @@ const edittodoListItem = function (e) {
         console.log(e);
 
         //make localStorage works
-        //check correct pathes
+        //check correct pathes for edit
         //localStorage.setItem("todos", JSON.stringify(toDoList));
+    });
+};
+
+const greetingTime = function () {
+    let time = date.getHours();
+    console.log(time);
+
+    greeting.innerHTML = "";
+    const html = `<h2 class="title">
+                        Good ${
+                            time >= 12 && time <= 18
+                                ? "afternoon"
+                                : time > 18 && time <= 24
+                                ? "evening"
+                                : "morning"
+                        },
+                            <input
+                                type="text"
+                                id="userName"
+                                placeholder="write your name here"
+                            />
+                    </h2>`;
+    greeting.insertAdjacentHTML("afterbegin", html);
+
+    const userName = document.querySelector("#userName");
+
+    //Store user name
+    userName.addEventListener("change", function (e) {
+        //console.log(e.target.value);
+        localStorage.setItem("username", JSON.stringify(e.target.value));
     });
 };
